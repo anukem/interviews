59cc69a1059ea371698e573fa59aca214fa1724a
Jib <Jibzade@gmail.com>
2023-02-13 10:10:08 -0500

    Update sort_the_students.py

--- sort_the_students/sort_the_students.py
+++ sort_the_students/sort_the_students.py
@@ -8,3 +8,12 @@ def sortTheStudents( score: List[List[int]], k: int) -> List[List[int]]:
 assert sortTheStudents([[10,6,9,1],[7,5,11,2],[4,8,3,15]], 2) == [[7,5,11,2],[10,6,9,1],[4,8,3,15]]
 assert sortTheStudents([[3,4],[5,6]], 0) == [[5,6],[3,4]]
 
+
+def sortTheStudentsV2(score: List[List[int]], k: int) -> List[List[int]]:
+      # For inplace sort...
+      # score.sort(key=lambda student: student[k], reverse=True)
+      # return score
+      return sorted(score, key=lambda student: student[k], reverse=True)
+
+assert sortTheStudentsV2([[10,6,9,1],[7,5,11,2],[4,8,3,15]], 2) == [[7,5,11,2],[10,6,9,1],[4,8,3,15]]
+assert sortTheStudentsV2([[3,4],[5,6]], 0) == [[5,6],[3,4]]
